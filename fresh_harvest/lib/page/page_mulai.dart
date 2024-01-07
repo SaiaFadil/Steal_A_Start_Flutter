61572403ee1b9c20f6b1b5771177430ee811f814
Fadil <fadillahwahyunugraha@gmail.com>
2024-01-07 20:39:26 +0700

    news

--- fresh_harvest/lib/page/page_mulai.dart
+++ fresh_harvest/lib/page/page_mulai.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fresh_harvest/page/page_login.dart';
+import 'package:fresh_harvest/src/CustomColors.dart';
+import 'package:fresh_harvest/src/CustomText.dart';
 
 class MulaiAplikasi extends StatefulWidget {
-  const MulaiAplikasi({super.key});
-
   @override
   State<MulaiAplikasi> createState() => _MulaiAplikasiState();
 }
@@ -11,9 +12,69 @@ class _MulaiAplikasiState extends State<MulaiAplikasi> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(body: Center(
-        child: Image.asset("assets/images/animmulai.png"),
-      )),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              right: 0,
+              child: Image.asset(
+                "assets/images/animmulai.png",
+                fit: BoxFit.cover,
+                width: MediaQuery.of(context).size.width,
+              ),
+            ),
+            Positioned(
+              top: 430,
+              left: 15,
+              right: 15,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text('GAYA HIDUP SEHAT',
+                        style: CustomText.TextChewyBold(
+                            28, CustomColors.blackColor)),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(30, 5, 30, 20),
+                    child: Text(
+                      "Dengan Selalu Konsumsi\nBuah dan Sayuran Segar",
+                      textAlign: TextAlign.center,
+                      style:
+                          CustomText.TextArvoBold(20, CustomColors.blackColor),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder: (_, __, ___) => page_login(),
+                          transitionsBuilder: (_, animation, __, child) {
+                            return FadeTransition(
+                              opacity: animation,
+                              child: child,
+                            );
+                          },
+                          transitionDuration: Duration(milliseconds: 1000),
+                        ),
+                      );
+                    },
+                    child: Image.asset(
+                      "assets/images/tombolmulai.png",
+                      // atur ukuran gambar sesuai kebutuhan
+                      width: 250,
+                      height: 150,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- fresh_harvest/lib/splashscreen.dart
+++ fresh_harvest/lib/splashscreen.dart
@@ -1,4 +1,3 @@
-import 'package:fresh_harvest/page/page_login.dart';
 import 'package:fresh_harvest/page/page_mulai.dart';
 import 'package:fresh_harvest/src/CustomColors.dart';
 import 'package:flutter/material.dart';
@@ -13,21 +12,20 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-Timer(Duration(seconds: 4), () {
-  Navigator.of(context).pushReplacement(
-    PageRouteBuilder(
-      pageBuilder: (_, __, ___) => MulaiAplikasi(),
-      transitionsBuilder: (_, animation, __, child) {
-        return FadeTransition(
-          opacity: animation,
-          child: child,
-        );
-      },
-      transitionDuration: Duration(milliseconds: 500),
-    ),
-  );
-});
-
+    Timer(Duration(seconds: 4), () {
+      Navigator.of(context).pushReplacement(
+        PageRouteBuilder(
+          pageBuilder: (_, __, ___) => MulaiAplikasi(),
+          transitionsBuilder: (_, animation, __, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+          transitionDuration: Duration(milliseconds: 500),
+        ),
+      );
+    });
   }
 
   @override
@@ -40,15 +38,13 @@ Timer(Duration(seconds: 4), () {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-         
               Container(
-                  margin: EdgeInsets.symmetric(vertical: 20,horizontal: 20),
+                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
                   child: Image.asset(
                     "assets/images/LogoSplashScreen.png",
                     alignment: Alignment.center,
                     width: double.infinity,
                   )),
-              
             ],
           ),
         ),
