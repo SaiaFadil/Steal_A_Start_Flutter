283fe5a219e2dad729dffd6f470cb663db2a0cd6
Fadil <fadillahwahyunugraha@gmail.com>
2024-01-11 11:35:39 +0700

    navbar done

--- fresh_harvest/lib/navigation/utama.dart
+++ fresh_harvest/lib/navigation/utama.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fresh_harvest/menu/menu_home.dart';
+import 'package:fresh_harvest/menu/menu_profile.dart';
+import 'package:fresh_harvest/menu/menu_shop.dart';
 import 'package:fresh_harvest/navigation/bottom_navbar.dart';
-import 'package:fresh_harvest/navigation/menu_home.dart';
-import 'package:fresh_harvest/navigation/menu_profile.dart';
-import 'package:fresh_harvest/navigation/menu_shop.dart';
 import 'package:fresh_harvest/src/CustomColors.dart';
+import 'package:fresh_harvest/src/CustomText.dart';
 
 class utama extends StatelessWidget {
   @override
@@ -39,7 +40,6 @@ class _utamaPageState extends State<utamaPage> {
     return BlocBuilder<BottomNavCubit, int>(
       builder: (context, state) {
         return Scaffold(
-          
           backgroundColor: CustomColors.whiteColor,
           body: _buildBody(state),
           bottomNavigationBar: _buildBottomNav(),
@@ -57,31 +57,43 @@ class _utamaPageState extends State<utamaPage> {
       elevation: 30.0,
       currentIndex: context.read<BottomNavCubit>().state,
       type: BottomNavigationBarType.fixed,
-      
+      selectedIconTheme:
+          IconThemeData(size: 20, color: CustomColors.primaryColor),
+      fixedColor: CustomColors.primaryColor,
+      unselectedItemColor: CustomColors.whiteColor,
+      landscapeLayout: BottomNavigationBarLandscapeLayout.linear,
+      backgroundColor: CustomColors.whiteColor,
+      unselectedLabelStyle: CustomText.TextArvo(1, CustomColors.whiteColor),
+      selectedLabelStyle: CustomText.TextArvo(13, CustomColors.primaryColor),
       onTap: _getChangeBottomNav,
       items: [
-        BottomNavigationBarItem(tooltip: "home",
+        BottomNavigationBarItem(
+            tooltip: "home",
             icon: Image.asset(
               "assets/images/nav_home.png",
               height: 25,
             ),
-            label: "Home",activeIcon: Image.asset("assets/images/nav_home_on.png",
-              height: 35)),
+            label: "Home",
+            activeIcon:
+                Image.asset("assets/images/nav_home_on.png", height: 35)),
         BottomNavigationBarItem(
-          tooltip: "Shop",
+            tooltip: "Shop",
             icon: Image.asset(
               "assets/images/nav_shop.png",
               height: 25,
             ),
-            label: "Shop",activeIcon: Image.asset("assets/images/nav_shop_on.png",
-              height: 35)),
+            label: "Shop",
+            activeIcon:
+                Image.asset("assets/images/nav_shop_on.png", height: 35)),
         BottomNavigationBarItem(
             icon: Image.asset(
               "assets/images/nav_profile.png",
               height: 25,
             ),
-            label: "Profile",activeIcon: Image.asset("assets/images/nav_profile_on.png",
-              height: 35),tooltip: "Profil"),
+            label: "Profile",
+            activeIcon:
+                Image.asset("assets/images/nav_profile_on.png", height: 35),
+            tooltip: "Profil"),
       ],
     );
   }
