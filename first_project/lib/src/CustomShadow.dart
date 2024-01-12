b605d64098dd4156dc34fee76c4f9d3897769f10
Fadil <fadillahwahyunugraha@gmail.com>
2024-01-12 17:33:26 +0700

    deleted project

--- first_project/lib/src/CustomShadow.dart
+++
@@ -1,6 +0,0 @@
-import 'package:first_project/src/CustomColors.dart';
-import 'package:flutter/material.dart';
-
-class CustomShadow{
-  static Shadow TextShadow=  Shadow(color: CustomColors.blackColor,blurRadius: 10);
-}
