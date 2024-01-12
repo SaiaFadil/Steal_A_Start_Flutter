a392eb53aa0e0f5bb6195f200312a3385de67a4a
Fadil <fadillahwahyunugraha@gmail.com>
2024-01-12 17:33:41 +0700

    deleted projects

--- nganjuk_elok/android/app/src/main/kotlin/com/example/nganjuk_elok/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.nganjuk_elok
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- nganjuk_elok/lib/res/CustomColors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-
-class CustomColors {
-  static Color GreenDark = Color.fromRGBO(12, 57, 36, 1);
-
-}
